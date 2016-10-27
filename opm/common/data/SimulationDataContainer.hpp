a6bb299110267a55bef7b503b74d2434c9a597f2
Fotios Karaoulanis <fotios.karaoulanis@gmail.com>
2016-10-27 19:55:08 +0300

    Fixed cpplint warnings. Introduced doxygen documentation.

--- opm/common/data/SimulationDataContainer.hpp
+++ opm/common/data/SimulationDataContainer.hpp
@@ -17,175 +17,296 @@
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef SIMULATION_DATA_CONTAINER_HPP
-#define SIMULATION_DATA_CONTAINER_HPP
+#ifndef OPM_COMMON_DATA_SIMULATIONDATACONTAINER_H_
+#define OPM_COMMON_DATA_SIMULATIONDATACONTAINER_H_
 
 #include <cstddef>
 #include <string>
 #include <map>
+#include <utility>
 #include <vector>
 
 namespace Opm {
+/**
+ * @class SimulationDataContainer
+ * @brief A simple container to manage simulation data.
+ * 
+ * The container is instantiated with information of how many cells,
+ * faces and phases are present in the reservoirmodel. 
+ * You can then add data to the container by using the
+ * 
+ * - registerCellData()
+ * - registerFaceData()
+ * 
+ * functions. The container owns and manages the data, but
+ * mutable references are returned with the getCellData() and
+ * getFaceData() methods, and the content will typically be
+ * modified by external scope.
+ */
+class SimulationDataContainer {
+ public:
+  /**
+   * @brief Main constructor setting the sizes for the contained data types.
+   * @param num_cells number of elements in cell data vectors
+   * @param num_faces   number of elements in face data vectors
+   * @param num_phases  number of phases, the number of components
+   *                    in any data vector must equal 1 or this
+   *                    number
+   * @deprecated @p num_phases behaviour and argument
+   */
+  SimulationDataContainer(size_t num_cells, size_t num_faces,
+                          size_t num_phases);
 
+  /**
+   * @brief Copy constructor.
+   * 
+   * Must be defined explicitly because class contains non-value objects (the 
+   * reference pointers pressure_ref_ etc.) that should not simply be copied.
+   */
+  SimulationDataContainer(const SimulationDataContainer&);
 
-    /// The SimulationDataContainer is a simple container to manage
-    /// simulation data. The container is instantiated with information
-    /// of how many cells, faces and phases are present in the
-    /// reservoirmodel. You can then add data to the container by using the
-    ///
-    ///   registerCellData()
-    ///   registerFaceData()
-    ///
-    /// functions. The container owns and manages the data, but
-    /// mutable references are returned with the getCellData() and
-    /// getFaceData() methods, and the content will typically be
-    /// modified by external scope.
-    class SimulationDataContainer
-    {
-    public:
-        /// Main constructor setting the sizes for the contained data
-        /// types.
-        /// \param num_cells   number of elements in cell data vectors
-        /// \param num_faces   number of elements in face data vectors
-        /// \param num_phases  number of phases, the number of components
-        ///                    in any data vector must equal 1 or this
-        ///                    number (this behaviour and argument is deprecated).
-        SimulationDataContainer(size_t num_cells, size_t num_faces, size_t num_phases);
+  /**
+   * @brief Copy assignment operator.
+   * 
+   * Must be defined explicitly because class contains non-value objects (the 
+   * reference pointers pressure_ref_ etc.) that should not simply be copied.
+   */
+  SimulationDataContainer& operator=(const SimulationDataContainer&);
 
-        /// Copy constructor.
-        /// Must be defined explicitly because class contains non-value objects
-        /// (the reference pointers pressure_ref_ etc.) that should not simply
-        /// be copied.
-        SimulationDataContainer(const SimulationDataContainer&);
+  /**
+   * @brief  Efficient O(1) swap.
+   */
+  void swap(SimulationDataContainer&);
 
-        /// Copy assignment operator.
-        /// Must be defined explicitly because class contains non-value objects
-        /// (the reference pointers pressure_ref_ etc.) that should not simply
-        /// be copied.
-        SimulationDataContainer& operator=(const SimulationDataContainer&);
+  /**
+   * @brief Get the number of phases.
+   * @todo Inline this getter, it looks relatively cheap.
+   */
+  size_t numPhases() const;
 
-        /// Efficient O(1) swap.
-        void swap(SimulationDataContainer& other);
+  /**
+   * @brief Get the number of faces.
+   * @todo Inline this getter, it looks relatively cheap.
+   */
+  size_t numFaces() const;
 
-        size_t numPhases() const;
-        size_t numFaces() const;
-        size_t numCells() const;
+  /**
+   * @brief Get the number of cells.
+   * @todo Inline this getter, it looks relatively cheap.
+   */
+  size_t numCells() const;
 
-        bool hasCellData( const std::string& name ) const;
+  /**
+   * @brief Check whether a cell is in the container.
+   * @param name the name of the cell
+   * @return true if found
+   */
+  bool hasCellData(const std::string& name) const;
 
-        /// Will register a data vector of size numCells() *
-        /// components.
-        void registerCellData( const std::string& name , size_t components , double initialValue = 0.0 );
-        std::vector<double>& getCellData( const std::string& name );
-        const std::vector<double>& getCellData( const std::string& name ) const;
+  /**
+   * @brief Register a cell data vector of size numCells() * components.
+   * @param name the name of the data vector
+   * @param components the number of components related to each cell
+   * @param initialValue initialization value for the vector
+   */
+  void registerCellData(const std::string& name, size_t components,
+                        double initialValue = 0.0);
 
-        bool hasFaceData( const std::string& name ) const;
-        void registerFaceData( const std::string& name , size_t components , double initialValue = 0.0 );
-        std::vector<double>& getFaceData( const std::string& name );
-        const std::vector<double>& getFaceData( const std::string& name ) const;
+  /**
+   * @brief Retrieve a stored cell data vector 
+   * @param name the name of the vector
+   * @return a reference to a vector of size numCells() * components
+   */
+  std::vector<double>& getCellData(const std::string& name);
 
-        /// Will return the number of components of the celldata with
-        /// name @name:
-        ///
-        ///    numCellDataComponents( "PRESSURE" )   -> 1
-        ///    numCellDataComponents( "SATURATION" ) -> 3
-        ///
-        /// for a three phase model.
-        size_t numCellDataComponents( const std::string& name ) const;
-        bool equal(const SimulationDataContainer& other) const;
+  /**
+   * @brief Retrieve a stored cell data vector 
+   * @param name the name of the vector
+   * @return a const reference to a vector of size numCells() * components
+   */
+  const std::vector<double>& getCellData(const std::string& name) const;
 
+  /**
+   * @brief Check whether a face is in the container.
+   * @param name the name of the face
+   * @return true if found
+   */
+  bool hasFaceData(const std::string& name) const;
 
-        /// Will set the values of component nr @component in the
-        /// field @key. All the cells in @cells will be set to the
-        /// values in @values.
-        void setCellDataComponent( const std::string& key , size_t component , const std::vector<int>& cells , const std::vector<double>& values);
+  /**
+   * @brief Register a face data vector of size numCells() * components.
+   * @param name the name of the data vector
+   * @param components the number of components related to each face
+   * @param initialValue initialization value for the vector
+   */
+  void registerFaceData(const std::string& name, size_t components,
+                        double initialValue = 0.0);
 
-        // Direct explicit field access for certain default fields.
-        // These methods are all deprecated, and will eventually be moved to
-        // concrete subclasses.
+  /**
+   * @brief Retrieve a stored face data vector 
+   * @param name the name of the vector
+   * @return a reference to a vector of size numCells() * components
+   */
+  std::vector<double>& getFaceData(const std::string& name);
 
-        std::vector<double>& pressure    ();
-        std::vector<double>& temperature ();
-        std::vector<double>& saturation  ();
+  /**
+   * @brief Retrieve a stored face data vector 
+   * @param name the name of the vector
+   * @return a const reference to a vector of size numCells() * components
+   */
+  const std::vector<double>& getFaceData(const std::string& name) const;
 
-        std::vector<double>& facepressure();
-        std::vector<double>& faceflux    ();
+  /**
+   * @brief Return the number of components of the cell data vector.
+   * 
+   * The number of components are defined as:
+   * - <tt>numCellDataComponents("PRESSURE")   -> 1</tt>
+   * - <tt>numCellDataComponents("SATURATION") -> 3</tt>
+   * 
+   * for a three phase model.
+   * @param name The name of the cell data vector
+   */
+  size_t numCellDataComponents(const std::string& name) const;
 
-        const std::vector<double>& pressure    () const;
-        const std::vector<double>& temperature () const;
-        const std::vector<double>& saturation  () const;
+  /**
+   * @brief Check for equality between two containers
+   * @param other the other container to be tested
+   * @return true if equal
+   */
+  bool equal(const SimulationDataContainer& other) const;
 
-        const std::vector<double>& facepressure() const;
-        const std::vector<double>& faceflux    () const;
+  /**
+   * @brief Set values in a cell data vector
+   * @param key the name of the cell data vector
+   * @param component the component to be modified
+   * @param cells cell indices to be modified
+   * @param values new values to be assigned
+   */
+  void setCellDataComponent(const std::string& key, size_t component,
+                            const std::vector<int>& cells,
+                            const std::vector<double>& values);
 
-        const std::map<std::string, std::vector<double>>& cellData() const;
-        std::map<std::string, std::vector<double>>& cellData();
+  /**
+   * @brief Get pressure (mutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline std::vector<double>& SimulationDataContainer::pressure() {
+    return *pressure_ref_;
+  }
 
-    private:
-        void addDefaultFields();
-        void setReferencePointers();
+  /**
+   * @brief Get temperature (mutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline std::vector<double>& SimulationDataContainer::temperature() {
+    return *temperature_ref_;
+  }
 
-        size_t m_num_cells;
-        size_t m_num_faces;
-        size_t m_num_phases;
+  /**
+   * @brief Get saturation (mutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline std::vector<double>& SimulationDataContainer::saturation() {
+    return *saturation_ref_;
+  }
 
-        std::map< std::string , std::vector<double> > m_cell_data;
-        std::map< std::string , std::vector<double> > m_face_data;
+  /**
+   * @brief Get facepressure (mutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline std::vector<double>& SimulationDataContainer::facepressure() {
+    return *facepressure_ref_;
+  }
 
-        std::vector<double>* pressure_ref_;
-        std::vector<double>* temperature_ref_;
-        std::vector<double>* saturation_ref_;
-        std::vector<double>* facepressure_ref_;
-        std::vector<double>* faceflux_ref_;
-    };
+  /**
+   * @brief Get faceflux (mutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline std::vector<double>& SimulationDataContainer::faceflux() {
+    return *faceflux_ref_;
+  }
 
+  /**
+   * @brief Get pressure (immutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline const std::vector<double>& SimulationDataContainer::pressure() const {
+      return *pressure_ref_;
+  }
 
-    // Inline implementations of the direct accessors required to guarantee
-    // performance.
+  /**
+   * @brief Get temperature (immutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline const std::vector<double>&
+      SimulationDataContainer::temperature() const {
+    return *temperature_ref_;
+  }
 
+  /**
+   * @brief Get saturation (immutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline const std::vector<double>&
+      SimulationDataContainer::saturation() const {
+    return *saturation_ref_;
+  }
 
-    inline std::vector<double>& SimulationDataContainer::pressure( ) {
-        return *pressure_ref_;
-    }
+  /**
+   * @brief Get facepressure (immutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline const std::vector<double>&
+      SimulationDataContainer::facepressure() const {
+    return *facepressure_ref_;
+  }
 
-    inline std::vector<double>& SimulationDataContainer::temperature() {
-        return *temperature_ref_;
-    }
+  /**
+   * @brief Get faceflux (immutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  inline const std::vector<double>&
+      SimulationDataContainer::faceflux() const {
+    return *faceflux_ref_;
+  }
 
-    inline std::vector<double>& SimulationDataContainer::saturation() {
-        return *saturation_ref_;
-    }
+  /**
+   * @brief Get cell data set (immutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  const std::map<std::string, std::vector<double>>& cellData() const;
 
-    inline std::vector<double>& SimulationDataContainer::facepressure() {
-        return *facepressure_ref_;
-    }
+  /**
+   * @brief Get cell data set (mutable)
+   * @deprecated will eventually be moved to concrete subclasses
+   */
+  std::map<std::string, std::vector<double>>& cellData();
 
-    inline std::vector<double>& SimulationDataContainer::faceflux() {
-        return *faceflux_ref_;
-    }
+ private:
+  /**
+   * @brief Adds default fields 
+   * @deprecated It should not be used any more.
+   */
+  void addDefaultFields();
 
-    inline const std::vector<double>& SimulationDataContainer::pressure( ) const {
-        return *pressure_ref_;
-    }
+  /**
+   * @brief Sets the reference pointers for the fast accessors,
+   *        
+   * The fields must be created first by copying or a call to
+   * addDefaultFields().
+   */
+  void setReferencePointers();
 
-    inline const std::vector<double>& SimulationDataContainer::temperature() const {
-        return *temperature_ref_;
-    }
-
-    inline const std::vector<double>& SimulationDataContainer::saturation() const {
-        return *saturation_ref_;
-    }
-
-    inline const std::vector<double>& SimulationDataContainer::facepressure() const {
-        return *facepressure_ref_;
-    }
-
-    inline const std::vector<double>& SimulationDataContainer::faceflux() const {
-        return *faceflux_ref_;
-    }
-
-
-
-}
-
-#endif
+  size_t m_num_cells;  //!< number of cells
+  size_t m_num_faces;  //!< number of faces
+  size_t m_num_phases;  //!< number of phases
+  std::map< std::string, std::vector<double> > m_cell_data;  //!< cell data set
+  std::map< std::string, std::vector<double> > m_face_data;  //!< face data set
+  std::vector<double>* pressure_ref_;  //!< the pressure
+  std::vector<double>* temperature_ref_;  //!< the temperature
+  std::vector<double>* saturation_ref_;  //!< the saturation
+  std::vector<double>* facepressure_ref_;  //!< the face pressure
+  std::vector<double>* faceflux_ref_;  //!< the face flux
+};
+}  // namespace Opm
+#endif  // OPM_COMMON_DATA_SIMULATIONDATACONTAINER_H_
