4ad97e93ec182cfef321c964d9e92130bb5461fd
Fotios Karaoulanis <fotios.karaoulanis@gmail.com>
2016-10-30 17:12:41 +0200

    Fixed cpplint/g++ warnings.

--- opm/common/OpmLog/Logger.cpp
+++ opm/common/OpmLog/Logger.cpp
@@ -16,109 +16,113 @@
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#include "opm/common/OpmLog/Logger.hpp"
+#include <iostream>
+#include <memory>
 #include <sstream>
 #include <stdexcept>
-#include <iostream>
-
-#include <opm/common/OpmLog/LogBackend.hpp>
-#include <opm/common/OpmLog/Logger.hpp>
-#include <opm/common/OpmLog/LogUtil.hpp>
+#include <string>
+#include "opm/common/OpmLog/LogBackend.hpp"
+#include "opm/common/OpmLog/LogUtil.hpp"
 
 namespace Opm {
 
-    Logger::Logger()
-        : m_globalMask(0),
-          m_enabledTypes(0)
-    {
-        addMessageType( Log::MessageType::Debug , "debug");
-        addMessageType( Log::MessageType::Info , "info");
-        addMessageType( Log::MessageType::Warning , "warning");
-        addMessageType( Log::MessageType::Error , "error");
-        addMessageType( Log::MessageType::Problem , "problem");
-        addMessageType( Log::MessageType::Bug , "bug");
-        addMessageType( Log::MessageType::Note , "note");
-    }
-
-    void Logger::addTaggedMessage(int64_t messageType, const std::string& tag, const std::string& message) const {
-        if ((m_enabledTypes & messageType) == 0)
-            throw std::invalid_argument("Tried to issue message with unrecognized message ID");
-
-        if (m_globalMask & messageType) {
-            for (auto iter : m_backends) {
-                LogBackend& backend = *(iter.second);
-                backend.addTaggedMessage( messageType, tag, message );
-            }
-        }
-    }
-
-    void Logger::addMessage(int64_t messageType , const std::string& message) const {
-        addTaggedMessage(messageType, "", message);
-    }
-
-
-    void Logger::updateGlobalMask( int64_t mask ) {
-        m_globalMask |= mask;
-    }
-
-
-    bool Logger::hasBackend(const std::string& name) {
-        if (m_backends.find( name ) == m_backends.end())
-            return false;
-        else
-            return true;
-    }
-
-    void Logger::removeAllBackends() {
-        m_backends.clear();
-        m_globalMask = 0;
-    }
-
-    bool Logger::removeBackend(const std::string& name) {
-        size_t eraseCount = m_backends.erase( name );
-        if (eraseCount == 1)
-            return true;
-        else
-            return false;
-    }
-
-
-    void Logger::addBackend(const std::string& name , std::shared_ptr<LogBackend> backend) {
-        updateGlobalMask( backend->getMask() );
-        m_backends[ name ] = backend;
-    }
-
-
-    int64_t Logger::enabledMessageTypes() const {
-        return m_enabledTypes;
-    }
-
-    //static: 
-    bool Logger::enabledMessageType( int64_t enabledTypes , int64_t messageType) {
-        if (Log::isPower2( messageType)) {
-            if ((messageType & enabledTypes) == 0)
-                return false;
-            else
-                return true;
-        } else
-            throw std::invalid_argument("The message type id must be ~ 2^n");
-    }
-
-
-    //static:
-    bool Logger::enabledDefaultMessageType( int64_t messageType) {
-        return enabledMessageType( Log::DefaultMessageTypes , messageType );
-    }
-
-    bool Logger::enabledMessageType( int64_t messageType) const {
-        return enabledMessageType( m_enabledTypes , messageType );
-    }
-
-
-    void Logger::addMessageType( int64_t messageType , const std::string& /* prefix */) {
-        if (Log::isPower2( messageType)) {
-            m_enabledTypes |= messageType;
-        } else
-            throw std::invalid_argument("The message type id must be ~ 2^n");
-    }
-
+Logger::Logger()
+  : m_globalMask(0),
+    m_enabledTypes(0),
+    m_backends()  {
+  addMessageType(Log::MessageType::Debug, "debug");
+  addMessageType(Log::MessageType::Info, "info");
+  addMessageType(Log::MessageType::Warning, "warning");
+  addMessageType(Log::MessageType::Error, "error");
+  addMessageType(Log::MessageType::Problem, "problem");
+  addMessageType(Log::MessageType::Bug, "bug");
+  addMessageType(Log::MessageType::Note, "note");
 }
+
+void Logger::addTaggedMessage(int64_t messageType, const std::string& tag,
+                              const std::string& message) const {
+  if ((m_enabledTypes & messageType) == 0) {
+      throw std::invalid_argument(
+        "Tried to issue message with unrecognized message ID");
+  }
+  if (m_globalMask & messageType) {
+    for (auto iter : m_backends) {
+      LogBackend& backend = *(iter.second);
+      backend.addTaggedMessage(messageType, tag, message);
+    }
+  }
+}
+
+void Logger::addMessage(int64_t messageType, const std::string& message) const {
+  addTaggedMessage(messageType, "", message);
+}
+
+void Logger::updateGlobalMask(int64_t mask) {
+  m_globalMask |= mask;
+}
+
+bool Logger::hasBackend(const std::string& name) {
+  if (m_backends.find(name) == m_backends.end()) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void Logger::removeAllBackends() {
+  m_backends.clear();
+  m_globalMask = 0;
+}
+
+bool Logger::removeBackend(const std::string& name) {
+  size_t eraseCount = m_backends.erase(name);
+  if (eraseCount == 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void Logger::addBackend(const std::string& name,
+                        std::shared_ptr<LogBackend> backend) {
+  updateGlobalMask(backend->getMask());
+  m_backends[ name ] = backend;
+}
+
+
+int64_t Logger::enabledMessageTypes() const {
+  return m_enabledTypes;
+}
+
+bool Logger::enabledMessageType(int64_t enabledTypes, int64_t messageType) {
+  if (Log::isPower2(messageType)) {
+    if ((messageType & enabledTypes) == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    throw std::invalid_argument("The message type id must be ~ 2^n");
+  }
+}
+
+bool Logger::enabledDefaultMessageType(int64_t messageType) {
+  return enabledMessageType(Log::DefaultMessageTypes, messageType);
+}
+
+bool Logger::enabledMessageType(int64_t messageType) const {
+  return enabledMessageType(m_enabledTypes , messageType);
+}
+
+
+void Logger::addMessageType(int64_t messageType,
+                            const std::string& /* prefix */) {
+  if (Log::isPower2(messageType)) {
+    m_enabledTypes |= messageType;
+  } else {
+    throw std::invalid_argument("The message type id must be ~ 2^n");
+  }
+}
+}  // namespace Opm
--- opm/common/OpmLog/Logger.hpp
+++ opm/common/OpmLog/Logger.hpp
@@ -28,55 +28,51 @@
 
 namespace Opm {
 
-    class LogBackend;
+class LogBackend;
 
 class Logger {
-
-public:
-    Logger();
-    void addMessage(int64_t messageType , const std::string& message) const;
-    void addTaggedMessage(int64_t messageType, const std::string& tag, const std::string& message) const;
-
-    static bool enabledDefaultMessageType( int64_t messageType);
-    bool enabledMessageType( int64_t messageType) const;
-    void addMessageType( int64_t messageType , const std::string& prefix);
-    int64_t enabledMessageTypes() const;
-
-    void addBackend(const std::string& name , std::shared_ptr<LogBackend> backend);
-    bool hasBackend(const std::string& name);
-    bool removeBackend(const std::string& name);
-    void removeAllBackends();
-
-    template <class BackendType>
-    std::shared_ptr<BackendType> getBackend(const std::string& name) const {
-        auto pair = m_backends.find( name );
-        if (pair == m_backends.end())
-            throw std::invalid_argument("Invalid backend name: " + name);
-        else
-            return std::static_pointer_cast<BackendType>(m_backends.find(name)->second);
+ public:
+  Logger();
+  void addMessage(int64_t messageType, const std::string& message) const;
+  void addTaggedMessage(int64_t messageType, const std::string& tag,
+                        const std::string& message) const;
+  static bool enabledDefaultMessageType(int64_t messageType);
+  bool enabledMessageType(int64_t messageType) const;
+  void addMessageType(int64_t messageType, const std::string& prefix);
+  int64_t enabledMessageTypes() const;
+  void addBackend(const std::string& name, std::shared_ptr<LogBackend> backend);
+  bool hasBackend(const std::string& name);
+  bool removeBackend(const std::string& name);
+  void removeAllBackends();
+  template <class BackendType>
+  std::shared_ptr<BackendType> getBackend(const std::string& name) const {
+    auto pair = m_backends.find(name);
+    if (pair == m_backends.end()) {
+      throw std::invalid_argument("Invalid backend name: " + name);
+    } else {
+      return std::static_pointer_cast<BackendType>(
+        m_backends.find(name)->second);
     }
+  }
+  template <class BackendType>
+  std::shared_ptr<BackendType> popBackend(const std::string& name)  {
+      auto pair = m_backends.find(name);
+      if (pair == m_backends.end()) {
+          throw std::invalid_argument("Invalid backend name: " + name);
+      } else {
+          std::shared_ptr<LogBackend> backend = (*pair).second;
+          removeBackend(name);
+          return std::static_pointer_cast<BackendType>(backend);
+      }
+  }
 
-    template <class BackendType>
-    std::shared_ptr<BackendType> popBackend(const std::string& name)  {
-        auto pair = m_backends.find( name );
-        if (pair == m_backends.end())
-            throw std::invalid_argument("Invalid backend name: " + name);
-        else {
-            std::shared_ptr<LogBackend> backend = (*pair).second;
-            removeBackend( name );
-            return std::static_pointer_cast<BackendType>(backend);
-        }
-    }
+ private:
+  void updateGlobalMask(int64_t mask);
+  static bool enabledMessageType(int64_t enabledTypes, int64_t messageType);
 
-
-private:
-    void updateGlobalMask( int64_t mask );
-    static bool enabledMessageType( int64_t enabledTypes , int64_t messageType);
-
-    int64_t m_globalMask;
-    int64_t m_enabledTypes;
-    std::map<std::string , std::shared_ptr<LogBackend> > m_backends;
+  int64_t m_globalMask;
+  int64_t m_enabledTypes;
+  std::map<std::string, std::shared_ptr<LogBackend> > m_backends;
 };
-
-}
+}  // namespace Opm
 #endif
